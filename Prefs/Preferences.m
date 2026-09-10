d8f35c5a41ed34ac6143c8f774dd06f5c8c32425
q1643240 <q1643240@users.noreply.github.com>
2026-09-10 14:07:24 +0800

    Make preferences controller ARC compliant

--- Prefs/Preferences.m
+++ Prefs/Preferences.m
@@ -49,7 +49,7 @@
 
 -(id)specifiers {
     if(_specifiers == nil) {
-        _specifiers = [[self loadSpecifiersFromPlistName:@"Prefs" target:self] retain];
+        _specifiers = [self loadSpecifiersFromPlistName:@"Prefs" target:self];
     }
     return _specifiers;
 }
